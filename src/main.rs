51dd55464f7fa9d95471ae6576928bbde1c8658e
ggxerus <ggxerus@gmail.com>
2025-10-04 21:44:52 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
+use std::ffi::c_void;
+
+type HANDLE = *mut c_void;
+type LPVOID = *mut c_void;
+type SIZE_T = usize;
+type DWORD = u32;
+
 extern "C" {
-    fn is_prime(n: u64) -> bool;
+    fn AllocMemory(size: SIZE_T, protect: DWORD) -> LPVOID;
+    fn FreeMemory(addr: LPVOID) -> i32;
+    fn WriteMemory(process: HANDLE, addr: LPVOID, buffer: LPVOID, size: SIZE_T, written: *mut SIZE_T) -> i32;
+    fn ReadMemory(process: HANDLE, addr: LPVOID, buffer: LPVOID, size: SIZE_T, read: *mut SIZE_T) -> i32;
+
+    fn RemoteAlloc(process: HANDLE, size: SIZE_T, protect: DWORD) -> LPVOID;
+    fn RemoteFree(process: HANDLE, addr: LPVOID) -> i32;
+    fn RemoteThread(process: HANDLE, addr: LPVOID, param: LPVOID) -> HANDLE;
+
+    fn OpenProc(access: DWORD, inherit: i32, pid: DWORD) -> HANDLE;
+    fn CloseHandleSafe(handle: HANDLE) -> i32;
+
+    fn ProtectMemory(addr: LPVOID, size: SIZE_T, newProtect: DWORD, oldProtect: *mut DWORD) -> i32;
+    fn ProtectMemoryEx(process: HANDLE, addr: LPVOID, size: SIZE_T, newProtect: DWORD, oldProtect: *mut DWORD) -> i32;
+
+    fn LoadLib(name: *const i8) -> HANDLE;
+    fn GetProc(module: HANDLE, name: *const i8) -> HANDLE;
+
+    fn Suspend(thread: HANDLE) -> i32;
+    fn Resume(thread: HANDLE) -> i32;
+    fn WaitHandle(handle: HANDLE, ms: DWORD) -> DWORD;
 }
 
 fn main() {
-    println!("Enter a number:");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let num: u64 = input.trim().parse().unwrap();
-
     unsafe {
-        if is_prime(num) {
-            println!("{} is prime!", num);
-        } else {
-            println!("{} is NOT prime!", num);
+        let mem = AllocMemory(1024, 0x04);
+        if !mem.is_null() {
+            println!("Allocated at {:?}", mem);
+            FreeMemory(mem);
         }
     }
 }
